b0f53e0175f1f79a30a1d85b4972eae1ad06737b
Michael Epping <michael@rvrwest.com>
2025-05-16 12:14:57 -0700

    Initial Commit

---
+++ PSSODeviceIdQueryObjCApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PSSODeviceIdQueryObjCApp
+//
+//  Created by Michael Epping on 5/16/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
